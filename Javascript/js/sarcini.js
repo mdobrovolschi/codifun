ecc9832fdd8972b5e641ed0c0d75792a2e7d8039
mdobrovolschi <anairam88mgmail.com>
2019-12-29 12:35:07 +0200

    Add Javascript practice

---
+++ Javascript/js/sarcini.js
@@ -0,0 +1,12 @@
+var informatie="În Republica Moldova Crăciunul este sărbătorit în data de 25/12 și în data de 07/01.";
+// aici se afiseaza continutul variabilei 'informatie'
+//var lungime=informatie.length;
+var infoNou=informatie.replace("25/12" , "25 decembrie");
+infoNou=informatie.replace('07/01' , '7 ianuarie')
+alert(infoNou);
+//alert ("Lungimea sirului textual cu denumirea 'informatie' este : "  +  informatie.length);
+//alert(informatie);
+// var nr1, nr2;
+// nr1=7
+// nr2=nr1*2+15;
+// alert('Rezultatul expresiei calculate este: ' + nr2);
---
+++ Javascript/js/variabile.js
@@ -0,0 +1,9 @@
+var nume="Ivanov",prenume="Ivan",varsta=35;  //definesc si initializez 3 variabile
+alert(nume+" "+prenume+" "+varsta);   //afisez valorile celor 3 variabile definite
+var salariu, premiu;    //definesc inca 2 variabile
+salariu=4000;   //initializez variabila salariu
+premiu=500;   //initializez variabila premiu
+salariu=salariu+2*premiu;   //calculez noua valoare a variabilei salariu
+var impozit=300;   //definesc si initializez variabila impozit
+salariu-=impozit;   //calculez care va fi salariu fara impozit
+alert(nume + ' are salariu de ' + salariu);   //afisez ce salariu are angajatul
